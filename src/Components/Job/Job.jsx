c186ddb636f4c4d7e6302c0f0e0547c2ea36c344
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-23 19:06:54 +0600

    style featured job card

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,42 @@
+import PropTypes from "prop-types";
 const Job = ({ job }) => {
-  const { logo, job_title } = job;
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+  } = job;
   return (
-    <div>
-      <h2>{job_title}</h2>
-      <img src={logo} alt="" />
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src={logo} alt="logo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div className="flex gap-4">
+          <button className="btn bg-white text-[#7E90FE] px-5 py-2 font-extrabold rounded duration-700 border-white hover:border-[#7E90FE] hover:bg-white">
+            {remote_or_onsite}
+          </button>
+          <button className="btn bg-white text-[#7E90FE] px-5 py-2 font-extrabold rounded duration-700 border-white hover:border-[#7E90FE] hover:bg-white">
+            {job_type}
+          </button>
+        </div>
+        <div className="card-actions">
+          <button className="btn btn-primary hover:bg-[#556bff] duration-700 text-white">
+            View Details
+          </button>
+        </div>
+      </div>
     </div>
   );
+};
+
+Job.propTypes = {
+  job: PropTypes.object,
 };
 
 export default Job;
